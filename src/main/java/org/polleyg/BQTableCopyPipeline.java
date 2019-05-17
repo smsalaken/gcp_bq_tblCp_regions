1d14f58c295972c0a8888ea9de1dcd85c0ee45d0
Syed Salaken <salaken@10-108-23-116.wifi-g.deakin.edu.au>
2019-05-17 13:10:26 +1000

    first commit - copying Nasdaq code to Sydney

---
+++ src/main/java/org/polleyg/BQTableCopyPipeline.java
@@ -0,0 +1,364 @@
+package org.polleyg;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
+import com.google.cloud.bigquery.BigQueryException;
+import com.google.cloud.bigquery.TableId;
+import com.google.cloud.storage.StorageException;
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.*;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
+import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED;
+import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition.CREATE_NEVER;
+
+/**
+ * This application is designed to be used when you need to copy/transfer a BigQuery table(s) between location/region
+ * e.g. copying a table from the US to the EU. If you are just copying a table(s) between the same location/region, then
+ * you don't need this. Instead just use the `gcloud` CLI tool, WebUI, or the BigQuery API. Refer to the README.md for
+ * details/instructions on how to use this application.
+ *
+ * @author Graham Polley, Matthew Grey
+ */
+public class BQTableCopyPipeline {
+    private static final Logger LOG = LoggerFactory.getLogger(BQTableCopyPipeline.class);
+    private static final String DEFAULT_NUM_WORKERS = "1";
+    private static final String DEFAULT_MAX_WORKERS = "3";
+    private static final String DEFAULT_TYPE_WORKERS = "n1-standard-1";
+    private static final String DEFAULT_TARGET_LOCATION = null;
+    private static final String DEFAULT_ZONE = "australia-southeast1-a";
+    private static final String DEFAULT_WRITE_DISPOSITION = "truncate";
+    private static final String DEFAULT_DETECT_SCHEMA = "true";
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        new BQTableCopyPipeline().copy(args);
+    }
+
+    /**
+     * Kicks off the copying process by reading the YAML config and creating the initial DataflowPipelineOptions
+     * that will be used by all subsequent Dataflow pipelines. Each pipeline will share the same project and runner,
+     * but each pipeline can be configured differently, depending on user requirements.
+     *
+     * @param args
+     * @throws Exception
+     */
+    private void copy(final String[] args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        Config config = mapper.readValue(
+                new File(getClass().getClassLoader().getResource("config.yaml").getFile()),
+                new TypeReference<Config>() {
+                });
+        PipelineOptionsFactory.register(DataflowPipelineOptions.class);
+        DataflowPipelineOptions options = PipelineOptionsFactory
+                .fromArgs(args)
+                .as(DataflowPipelineOptions.class);
+        if (config.copies == null || config.copies.size() == 0) {
+            throw new IllegalStateException("No table or datasets were defined for copying in the config file");
+        }
+        options.setProject(config.project);
+        options.setRunner(GCPHelpers.getRunnerClass(config.runner));
+
+        LOG.info("BigQuery table copy: {}", config);
+
+        List<Map<String, String>> copyTables = new ArrayList<>();
+        for (Map<String, String> copy : config.copies) {
+            if (GCPHelpers.isDatasetTableSpec(copy.get("source"))) {
+                List<TableId> tableIds = GCPHelpers.getTableIds(copy.get("source"));
+                tableIds.forEach(id -> copyTables.add(createTableCopyParams(GCPHelpers.getTableIdAsString(id), copy)));
+            } else {
+                copyTables.add(copy);
+            }
+            copyTables.forEach(tableCopyParams -> setupAndRunPipeline(options, Arrays.asList(tableCopyParams), config));
+        }
+    }
+
+    /**
+     * Bootstraps the Dataflow pipeline with the provided configuration. For example, sets the number of Dataflow
+     * workers, zone, machine type etc. This can be configured per pipeline that does the copy. Finally, it makes
+     * a call to run the actual Dataflow pipeline.
+     *
+     * @param options         the options used for creating the actual Dataflow pipeline
+     * @param tableCopyParams a list of table copy Maps that encapsulates the copy configuration, which is defined in the YAML config
+     * @param config          the YAML config, used for globally-set copy params
+     */
+    private void setupAndRunPipeline(final DataflowPipelineOptions options,
+                                     final List<Map<String, String>> tableCopyParams,
+                                     final Config config) {
+
+        Map<String, String> pipeParams = getFullTableCopyParams(tableCopyParams.get(0), config); //use first copy command as base params for pipeline
+
+        String exportBucket = format("%s_df_bqcopy_export_%s", options.getProject(), pipeParams.get("sourceLocation")).toLowerCase();
+        String importBucket = format("%s_df_bqcopy_import_%s", options.getProject(), pipeParams.get("targetLocation")).toLowerCase();
+
+        handleBucketCreation(exportBucket, pipeParams.get("sourceLocation"));
+        handleBucketCreation(importBucket, pipeParams.get("targetLocation"));
+
+        Pipeline pipeline = setupPipeline(options, pipeParams, exportBucket);
+
+        tableCopyParams.forEach(tableCopy -> {
+
+            tableCopy = getFullTableCopyParams(tableCopy, config);
+
+            handleTargetDatasetCreation(tableCopy.get("target"), tableCopy.get("targetDatasetLocation"));
+
+            TableSchema schema = null; //no schema is permitted
+            if (Boolean.valueOf(tableCopy.get("detectSchema"))) {
+                schema = GCPHelpers.getTableSchema(tableCopy.get("source"));
+            }
+            WriteDisposition writeDisposition = GCPHelpers.getWriteDisposition(tableCopy.get("writeDisposition"));
+            addCopyToPipeline(pipeline, tableCopy.get("source"), tableCopy.get("target"), importBucket, schema, writeDisposition);
+        });
+        pipeline.run();
+    }
+
+    /**
+     * Creates a Dataflow pipeline based on the configuration parameters
+     *
+     * @param options        the options used for creating the actual Dataflow pipeline
+     * @param pipelineParams a Map that encapsulates the copy configuration, which is defined in the YAML config
+     * @param exportBucket   the GCS bucket name that is to be used in the exporting process
+     * @return the created Dataflow Pipeline
+     */
+    private Pipeline setupPipeline(final DataflowPipelineOptions options,
+                                   final Map<String, String> pipelineParams,
+                                   final String exportBucket) {
+
+        LOG.info("Running a copy for '{}'", pipelineParams);
+        int numWorkers = Integer.valueOf(pipelineParams.get("numWorkers"));
+        int maxNumWorkers = Integer.valueOf(pipelineParams.get("maxNumWorkers"));
+        String zone = pipelineParams.get("zone");
+        String worker = pipelineParams.get("workerMachineType");
+
+        options.setNumWorkers(numWorkers);
+        options.setMaxNumWorkers(maxNumWorkers);
+        options.setZone(zone);
+        options.setWorkerMachineType(worker);
+        options.setTempLocation(format("gs://%s/tmp", exportBucket));
+        options.setStagingLocation(format("gs://%s/jars", exportBucket));
+        options.setJobName(format("bq-copy-%s-to-%s-%d", pipelineParams.get("sourceLocation"), pipelineParams.get("targetLocation"), currentTimeMillis()));
+
+        LOG.info("Running Dataflow pipeline with options '{}'", options);
+
+        return Pipeline.create(options);
+    }
+
+    /**
+     * Adds a table-copy job to a Dataflow pipeline
+     *
+     * @param pipeline         the schema of the BigQuery table to use and can be null
+     * @param sourceTable      the source BigQuery to copy from
+     * @param targetTable      the target BigQuery to copy to
+     * @param importBucket     the GCS bucket name that is to be used in the importing process
+     * @param schema           the schema of the table to be copied (null acceptable)
+     * @param writeDisposition the write disposition if the table already exists
+     */
+    private void addCopyToPipeline(final Pipeline pipeline,
+                                   final String sourceTable,
+                                   final String targetTable,
+                                   final String importBucket,
+                                   final TableSchema schema,
+                                   final WriteDisposition writeDisposition) {
+
+        checkNotNull(sourceTable, "Source table cannot be null");
+        checkNotNull(targetTable, "Target table cannot be null");
+
+        PCollection<TableRow> rows = pipeline.apply(format("Read: %s", sourceTable), BigQueryIO.readTableRows().from(sourceTable));
+        if (schema != null) {
+            rows.apply(format("Write: %s", targetTable), BigQueryIO.writeTableRows()
+                    .to(targetTable)
+                    .withCreateDisposition(CREATE_IF_NEEDED)
+                    .withWriteDisposition(writeDisposition)
+                    .withSchema(schema)
+                    .withCustomGcsTempLocation(StaticValueProvider.of((format("gs://%s", importBucket)))));
+        } else {
+            rows.apply(format("Write: %s", targetTable), BigQueryIO.writeTableRows()
+                    .to(targetTable)
+                    .withCreateDisposition(CREATE_NEVER)
+                    .withWriteDisposition(writeDisposition)
+                    .withCustomGcsTempLocation(StaticValueProvider.of((format("gs://%s", importBucket)))));
+        }
+    }
+
+    /**
+     * Wraps the creation of the GCS bucket in a try/catch block. If the bucket already exists it will swallow up the
+     * exception because that's ok. Otherwise, it will rethrow it.
+     *
+     * @param name     the name of the GCS bucket
+     * @param location the location of the GCS bucket e.g. "US"
+     */
+    private void handleBucketCreation(final String name,
+                                      final String location) {
+        try {
+            GCPHelpers.createGCSBucket(name, location);
+        } catch (StorageException e) {
+            if (e.getCode() != HttpStatus.SC_CONFLICT) { // 409 == bucket already exists. That's ok.
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+
+    /**
+     * Determines the location of the target dataset. If it has not configured in the YAML config, then it is assumed
+     * the target dataset exists. If it doesn't, then it will bail out and throw an exception. If it has been
+     * configured in the YAML config, then it will use this as the target dataset location. handleTargetDatasetCreation
+     * performs an existance check for the dataset in the target location.
+     *
+     * @param targetTable           the full table spec of the target table in format [PROJECT]:[DATASET].[TABLE]
+     * @param targetDatasetLocation the target dataset location and can be null
+     * @return the location of the dataset
+     */
+    private String getTargetDatasetLocation(final String targetTable,
+                                            final String targetDatasetLocation) {
+        String location;
+        if (targetDatasetLocation == null) {
+            //target dataset/table should already exist in this case
+            try {
+                location = GCPHelpers.getDatasetLocation(targetTable);
+            } catch (RuntimeException e) {
+                throw new IllegalStateException("'targetDatasetLocation' wasn't specified in config, but it looks" +
+                        " like the target dataset doesn't exist.");
+            }
+        } else {
+            //otherwise, return target dataset location
+            location = targetDatasetLocation;
+        }
+        assert location != null;
+        return location;
+    }
+
+    /**
+     * Handles the creation of the target dataset in a given location. If the target dataset location is specified
+     * in the YAML configuration, then it will attempt to verify that the dataset does not exist. If it does not
+     * exist, it will attempt to create the dataset in BigQuery using that region. If the dataset already exists and
+     * the target dataset location is specified in the YAML, then it will bail out and throw an exception. If the
+     * target dataset location is not set in the YAML, it will verify that the dataset exists within the project.
+     * If it does not exist, it will bail out and throw an exception
+     *
+     * @param targetTable           the full table spec of the target table in format [PROJECT]:[DATASET].[TABLE]
+     * @param targetDatasetLocation the target dataset location and can be null
+     */
+    private void handleTargetDatasetCreation(final String targetTable,
+                                             final String targetDatasetLocation) {
+        if (targetDatasetLocation == null) {
+            //target dataset/table should already exist in this case
+            try {
+                GCPHelpers.getDatasetLocation(targetTable);
+            } catch (RuntimeException e) {
+                throw new IllegalStateException("'targetDatasetLocation' wasn't specified in config, but it looks" +
+                        " like the target dataset doesn't exist.");
+            }
+        } else {
+            //otherwise, return target dataset location
+            try {
+                GCPHelpers.createBQDataset(targetTable, targetDatasetLocation);
+            } catch (BigQueryException e) {
+                if (e.getCode() == HttpStatus.SC_CONFLICT) { // 409 == dataset already exists
+                    throw new IllegalStateException(
+                            format("'targetDatasetLocation' specified in config, but the dataset '%s' already exists",
+                                    targetTable));
+                } else {
+                    throw new IllegalStateException(e);
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a map of params for a single table copy pipeline, data is combined from dataset copy config & a table that was found inside the dataset
+     *
+     * @param tableId           the full table spec of the target table in format [PROJECT]:[DATASET].[TABLE]
+     * @param datasetCopyParams a configuration map
+     * @return a map of copy parameters for one table copy
+     */
+    private Map<String, String> createTableCopyParams(String tableId, Map<String, String> datasetCopyParams) {
+        Map<String, String> params = new HashMap<>(datasetCopyParams);
+        params.put("source", tableId);
+        String tableName = tableId.split("\\.")[1];
+        params.put("target", format("%s.%s", datasetCopyParams.get("target"), tableName));
+        return params;
+    }
+
+    /**
+     * Combines values in the config with values specified in a map, favouring the map values over the config values where they exist
+     *
+     * @param copyParams a map of copy parameters that is to override values in the config
+     * @param config     the config specified in the YAML file
+     * @return the combined map of parameters
+     */
+    private Map<String, String> getFullTableCopyParams(Map<String, String> copyParams, Config config) {
+        copyParams.put("detectSchema", copyParams.getOrDefault("detectSchema", config.detectSchema));
+        copyParams.put("numWorkers", copyParams.getOrDefault("numWorkers", config.numWorkers));
+        copyParams.put("maxNumWorkers", copyParams.getOrDefault("maxNumWorkers", config.maxNumWorkers));
+        copyParams.put("zone", copyParams.getOrDefault("zone", config.zone));
+        copyParams.put("workerMachineType", copyParams.getOrDefault("workerMachineType", config.workerMachineType));
+        copyParams.put("writeDisposition", copyParams.getOrDefault("writeDisposition", config.writeDisposition));
+        copyParams.put("targetDatasetLocation", copyParams.getOrDefault("targetDatasetLocation", config.targetDatasetLocation));
+        copyParams.put("targetLocation", getTargetDatasetLocation(copyParams.get("target"), copyParams.get("targetDatasetLocation")));
+        copyParams.put("sourceLocation", GCPHelpers.getDatasetLocation(copyParams.get("source")));
+        return copyParams;
+    }
+
+    /**
+     * POJO for YAML config
+     */
+    private static class Config {
+        @JsonProperty
+        public List<Map<String, String>> copies;
+        @JsonProperty
+        public String project;
+        @JsonProperty
+        public String runner;
+
+        @JsonProperty
+        public String workerMachineType = DEFAULT_TYPE_WORKERS;
+        @JsonProperty
+        public String numWorkers = DEFAULT_NUM_WORKERS;
+        @JsonProperty
+        public String maxNumWorkers = DEFAULT_MAX_WORKERS;
+        @JsonProperty
+        public String targetDatasetLocation = DEFAULT_TARGET_LOCATION;
+        @JsonProperty
+        public String zone = DEFAULT_ZONE;
+        @JsonProperty
+        public String writeDisposition = DEFAULT_WRITE_DISPOSITION;
+        @JsonProperty
+        public String detectSchema = DEFAULT_DETECT_SCHEMA;
+
+        @Override
+        public String toString() {
+            return "Config{" +
+                    "copies=" + copies +
+                    ", project='" + project + '\'' +
+                    ", runner='" + runner + '\'' +
+                    ", workerMachineType='" + workerMachineType + '\'' +
+                    ", numWorkers='" + numWorkers + '\'' +
+                    ", maxNumWorkers='" + maxNumWorkers + '\'' +
+                    ", targetDatasetLocation='" + targetDatasetLocation + '\'' +
+                    ", zone='" + zone + '\'' +
+                    ", writeDisposition='" + writeDisposition + '\'' +
+                    ", detectSchema='" + detectSchema + '\'' +
+                    '}';
+        }
+    }
+}
---
+++ src/main/java/org/polleyg/GCPHelpers.java
@@ -0,0 +1,203 @@
+package org.polleyg;
+
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableReference;
+import com.google.api.services.bigquery.model.TableSchema;
+import com.google.cloud.bigquery.*;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageClass;
+import com.google.cloud.storage.StorageException;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.ImmutableMap;
+import org.apache.beam.runners.dataflow.DataflowRunner;
+import org.apache.beam.runners.direct.DirectRunner;
+import org.apache.beam.sdk.PipelineRunner;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryHelpers;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static com.google.cloud.storage.BucketInfo.newBuilder;
+import static java.lang.String.format;
+
+/**
+ * Some package-private helper/convenience methods for talking with GCP services. Nothing to see here,
+ * move along please.
+ *
+ * @author Graham Polley
+ */
+class GCPHelpers {
+    private static final Logger LOG = LoggerFactory.getLogger(GCPHelpers.class);
+    private static final BigQuery BIGQUERY = BigQueryOptions.getDefaultInstance().getService();
+    private static final Storage STORAGE = StorageOptions.getDefaultInstance().getService();
+    private static final Map<String, StorageClass> BQ_LOCATION_TO_GCS_STORAGE_CLASS = ImmutableMap.of(
+            "us", StorageClass.MULTI_REGIONAL,
+            "eu", StorageClass.MULTI_REGIONAL,
+            "asia-northeast1", StorageClass.REGIONAL,
+            "australia-southeast1", StorageClass.REGIONAL,
+            "europe-west2", StorageClass.REGIONAL);
+
+    /**
+     * Retrieves the table ids of all tables within a given dataset
+     *
+     * @param datasetSpec the dataset spec in the format [PROJECT]:[DATASET]
+     * @return a list of table ids for the given dataset
+     */
+    static List<TableId> getTableIds(final String datasetSpec) {
+        TableReference tableReference = BigQueryHelpers.parseTableSpec(datasetSpec);
+        if (tableReference.getDatasetId() == null) {
+            throw new IllegalStateException(String.format("No dataset could be found for %s", datasetSpec));
+        }
+        LOG.debug("Discovering tables in the dataset: {}", datasetSpec);
+        DatasetId datasetId = DatasetId.of(tableReference.getProjectId(), tableReference.getDatasetId());
+        return StreamSupport
+                .stream(BIGQUERY.listTables(datasetId).iterateAll().spliterator(), false)
+                .map(table -> table.getTableId())
+                .collect(Collectors.toList());
+
+    }
+
+    /**
+     * Returns the TableId as Strings in the format [PROJECT]:[DATASET].[TABLE]
+     *
+     * @param tableId
+     * @return
+     */
+    static String getTableIdAsString(final TableId tableId) {
+        return format("%s:%s.%s", tableId.getProject(), tableId.getDataset(), tableId.getTable());
+    }
+
+    /**
+     * Works out the table schema of the provided BigQuery table spec. Note, it currently does not support
+     * complex schemas with nested structures.
+     *
+     * @param tableSpec the full table spec in the format [PROJECT].[DATASET].[TABLE]
+     * @return the TableSchema.
+     */
+    static TableSchema getTableSchema(final String tableSpec) {
+        LOG.debug("Fetching schema for '{}'", tableSpec);
+        TableReference ref = BigQueryHelpers.parseTableSpec(tableSpec);
+        TableId tableId = TableId.of(ref.getProjectId(), ref.getDatasetId(), ref.getTableId());
+        List<TableFieldSchema> fields = new ArrayList<>();
+        Schema realSchema = BIGQUERY.getTable(tableId).getDefinition().getSchema();
+        realSchema.getFields().forEach(f -> fields.add(
+                new TableFieldSchema().setName(f.getName()).setType(f.getType().name()))
+        );
+        return new TableSchema().setFields(fields);
+    }
+
+    /**
+     * Creates a GCS bucket
+     *
+     * @param bucketName the name of the bucket to create
+     * @param location   the location where the bucket should be created
+     * @throws StorageException
+     */
+    static void createGCSBucket(final String bucketName,
+                                final String location) throws StorageException {
+        LOG.debug("Requested to create bucket '{}' in location '{}'..", bucketName, location);
+        StorageClass storageClass = BQ_LOCATION_TO_GCS_STORAGE_CLASS.get(location);
+        STORAGE.create(newBuilder(bucketName)
+                .setStorageClass(storageClass)
+                .setLocation(location)
+                .build());
+        LOG.info("Successfully created bucket '{}' with storage class '{}' and in location '{}'",
+                bucketName, storageClass, location);
+    }
+
+    /**
+     * Creates a BigQuery dataset
+     *
+     * @param tableSpec the full table spec in the format [PROJECT].[DATASET].[TABLE]
+     * @param location  the location where the dataset should be created
+     * @throws BigQueryException
+     */
+    static void createBQDataset(final String tableSpec,
+                                final String location) throws BigQueryException {
+        TableReference ref = BigQueryHelpers.parseTableSpec(tableSpec);
+        LOG.debug("Requested to create dataset '{}' in location '{}'..", ref.getDatasetId(), location);
+        DatasetInfo datasetInfo = DatasetInfo.newBuilder(ref.getProjectId(), ref.getDatasetId())
+                .setLocation(location)
+                .build();
+        BIGQUERY.create(datasetInfo);
+        LOG.info("Successfully created dataset '{}' in location '{}'", ref.getDatasetId(), location);
+    }
+
+    /**
+     * Gets the location of a BigQuery dataset
+     *
+     * @param tableSpec the full table spec in the format [PROJECT].[DATASET].[TABLE]
+     * @return the location of the dataset
+     */
+    static String getDatasetLocation(final String tableSpec) {
+        LOG.debug("Fetching BigQuery dataset location for '{}'", tableSpec);
+        TableReference tableReference = BigQueryHelpers.parseTableSpec(tableSpec);
+        DatasetId datasetId = DatasetId.of(tableReference.getProjectId(), tableReference.getDatasetId());
+        return BIGQUERY.getDataset(datasetId).getLocation().toLowerCase();
+    }
+
+    /**
+     * Takes a String and returns the corresponding Dataflow runner class object. Expects one of either 'dataflow' or
+     * 'local'.
+     *
+     * @param clazz the type of Dataflow runner
+     * @return the class object for the Dataflow runner.
+     */
+    static Class<? extends PipelineRunner<?>> getRunnerClass(final String clazz) {
+        Class<? extends PipelineRunner<?>> result;
+        switch (clazz) {
+            case "dataflow": {
+                result = DataflowRunner.class;
+                break;
+            }
+            case "local": {
+                result = DirectRunner.class;
+                break;
+            }
+            default:
+                throw new IllegalArgumentException(format("I don't know this runner: '%s'." +
+                        " Use one of 'Dataflow' or 'Local'", clazz));
+        }
+        return result;
+    }
+
+    /**
+     * Takes a String and return the corresponding Write Disposition. Expects one of either 'truncate' or 'append'.
+     *
+     * @param writeDisposition
+     * @return the Write Disposition for the pipeline
+     */
+    static WriteDisposition getWriteDisposition(final String writeDisposition) {
+        WriteDisposition result;
+        switch (writeDisposition) {
+            case "truncate": {
+                result = WriteDisposition.WRITE_TRUNCATE;
+                break;
+            }
+            case "append": {
+                result = WriteDisposition.WRITE_APPEND;
+                break;
+            }
+            default:
+                throw new IllegalArgumentException(format("I don't know this write disposition: '%s'." +
+                        " Use one of 'truncate' or 'append'", writeDisposition));
+        }
+        return result;
+    }
+
+    /**
+     * Determines if the table spec ( [PROJECT]:[DATASET].[TABLE] ) is that of an entire dataset or a single table
+     *
+     * @param spec the table spec to test
+     * @return boolean true if spec is for a dataset
+     */
+    static Boolean isDatasetTableSpec(String spec) {
+        return BigQueryHelpers.parseTableSpec(spec).getTableId() == null;
+    }
+}
